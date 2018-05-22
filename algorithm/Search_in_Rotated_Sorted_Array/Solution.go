84cfed63c4a9ba55bf1bf7145f962a37cceff7b3
Chen Bin (chbin) <chbin@cisco.com>
2018-05-22 14:57:15 +0800

    Search in rotated sorted array : java/go

---
+++ algorithm/Search_in_Rotated_Sorted_Array/Solution.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+	"fmt"
+)
+
+func search(nums []int, target int) int {
+    if len(nums) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(nums) - 1
+	mid := -1
+
+	for ;left<=right; {
+		mid = left + (right + 1 - left) /2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	nums := []int{4,5,6,7,0,1,2}
+	fmt.Println(search(nums, 7))
+}
---
+++ algorithm/Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    
+    public int search(int[] nums, int target) {
+        if(nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        int mid = -1;
+        while(left <= right) {
+            mid = left + (right + 1 - left) / 2;
+            System.out.printf("left:%d right:%d mid:%d\n", left, right, mid);
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            if(nums[mid] >= nums[left]) {
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(nums[mid] < target && target <= nums[right])  {
+                    left = mid + 1;
+                } else {
+                    right = right -1;
+                }
+            }
+
+            
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution ins = new Solution();
+        int[] nums = {4, 5, 6, 7 , 0, 1, 2};
+        System.out.println(ins.search(nums, 4));
+        System.out.println(ins.search(nums, 3));
+        System.out.println(ins.search(nums, 0));
+        nums = new int[]{3, 1};
+        System.out.println(ins.search(nums, 3));
+        nums = new int[]{1, 3};
+        System.out.println(ins.search(nums, 1));
+        nums = new int[]{5, 1, 3};
+        System.out.println(ins.search(nums, 5));
+        nums = new int[]{7,8,1,2,3,4,5,6};
+        System.out.println(ins.search(nums, 2));
+    }
+}
