cf7b87987647cc389e552077372d5174e3085dd6
Chen Bin (chbin) <chbin@cisco.com>
2018-06-04 23:03:25 +0800

    Climbing Stairs : java

---
+++ algorithm/Climbing_Stairs/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        int result = 0;
+        int pre_1=1, pre_2=1;
+        for(int i=2;i<=n;i++) {
+            result = pre_1 + pre_2;
+            pre_2 = pre_1;
+            pre_1 = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution ins = new Solution();
+        System.out.println(ins.climbStairs(4));
+    }
+}
